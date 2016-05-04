ef5e5d951e8481b360d0ee4faa777deb786b9b3f
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-04 07:25:36 +0200

    refactor: extract method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,12 +2,16 @@ public class FizzBuzz {
     public String[] values() {
         String[] values = new String[100];
         for (int i = 0; i < 100; i++) {
-            if ((i +1) % 3 == 0) {
+            if (isFizz(i + 1)) {
                 values[i] = "Fizz";
             } else {
                 values[i] = String.valueOf(i + 1);
             }
         }
         return values;
+    }
+
+    private boolean isFizz(int number) {
+        return number % 3 == 0;
     }
 }
