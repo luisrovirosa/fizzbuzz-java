43cbdbf499179f491817b747f948974a46a70da1
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-04 07:24:48 +0200

    refactor: reduce duplicity

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,7 +2,7 @@ public class FizzBuzz {
     public String[] values() {
         String[] values = new String[100];
         for (int i = 0; i < 100; i++) {
-            if (i == 2 || i == 5 || i == 8) {
+            if ((i +1) % 3 == 0) {
                 values[i] = "Fizz";
             } else {
                 values[i] = String.valueOf(i + 1);
