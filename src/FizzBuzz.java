6ca138acbd76adc72281f57b7665bb5fc5bdb2a6
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-04 07:09:26 +0200

    should_contains_one_hundred_elements

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,2 +1,5 @@
 public class FizzBuzz {
+    public String[] values() {
+        return new String[100];
+    }
 }
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -4,5 +4,9 @@ import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
 
-
+    @Test
+    public void should_contains_one_hundred_elements(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals(100, fizzBuzz.values().length);
+    }
 }
