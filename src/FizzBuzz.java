1fc9c8b45596d94f035da2c470eb3cf11e09c9d6
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-04 07:23:46 +0200

    six_is_fizz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,7 +2,7 @@ public class FizzBuzz {
     public String[] values() {
         String[] values = new String[100];
         for (int i = 0; i < 100; i++) {
-            if (i == 2) {
+            if (i == 2 || i == 5) {
                 values[i] = "Fizz";
             } else {
                 values[i] = String.valueOf(i + 1);
--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -8,38 +8,43 @@ public class FizzBuzzTest {
     private String[] values;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         values = fizzBuzz.values();
     }
 
     @Test
-    public void should_contains_one_hundred_elements(){
+    public void should_contains_one_hundred_elements() {
         assertEquals(100, values.length);
     }
 
     @Test
-    public void one_is_one(){
+    public void one_is_one() {
         assertSameValue(1);
     }
+
     @Test
-    public void two_is_two(){
+    public void two_is_two() {
         assertSameValue(2);
     }
 
     @Test
-    public void four_is_four(){
+    public void four_is_four() {
         assertSameValue(4);
     }
 
     @Test
-    public void three_is_fizz(){
+    public void three_is_fizz() {
         assertEquals("Fizz", values[2]);
     }
 
+    @Test
+    public void six_is_fizz() {
+        assertEquals("Fizz", values[5]);
+    }
 
 
-    private void assertSameValue(int number){
+    private void assertSameValue(int number) {
         assertEquals(String.valueOf(number), values[number - 1]);
     }
 }
