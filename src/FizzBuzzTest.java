9288480dc315aacd367145534d47696733997d2c
Luis Rovirosa <luisrovirosa@gmail.com>
2016-05-04 07:14:59 +0200

    refactor: extract field

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -1,23 +1,28 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp(){
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void should_contains_one_hundred_elements(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals(100, fizzBuzz.values().length);
     }
 
     @Test
     public void one_is_one(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.values()[0]);
     }
     @Test
     public void two_is_two(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.values()[1]);
     }
 }
